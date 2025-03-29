1632ad8701e3efb7ff0a7b480240cb8c1b6cc0f3
RyanB1303 <ryan.brilliant.nirwana@gmail.com>
2025-03-29 07:11:30 +0700

    externalized configuration management (chapter 4)

---
+++ src/main/java/com/polarbookshop/catalog_service/demo/BookDataLoader.java
@@ -0,0 +1,26 @@
+package com.polarbookshop.catalog_service.demo;
+
+import com.polarbookshop.catalog_service.domain.Book;
+import com.polarbookshop.catalog_service.domain.BookRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("testdata")
+public class BookDataLoader {
+    private final BookRepository bookRepository;
+
+    public BookDataLoader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTestData() {
+        var book1 = new Book("1234567890", "Title", "Author", 9.99);
+        var book2 = new Book("1234567891", "Polar Journey", "Iorek", 5.99);
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+}
